71b5975fe0d09097c35e365503fa7d13be185e98
Umut Tosun <tosun.umutt@gmail.com>
2022-04-12 11:19:52 +0300

    Ödev yapıldı

---
+++ src/KosulluIfadelerVeKodBloklari/Giris.java
@@ -0,0 +1,36 @@
+package KosulluIfadelerVeKodBloklari;
+
+import java.util.Scanner;
+
+public class Giris {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String userName,password,newPassword;
+
+        System.out.print("Kullanıcı Adınız : ");
+        userName=input.next();
+
+        System.out.print("Şifreniz : ");
+        password=input.next();
+
+        if (userName.equals("patika")&&password.equals("java101"))
+            System.out.println("Giriş Başarılı");
+        else
+        {
+            if (password!="java101")
+            {
+                System.out.print("Şifreniz hatalı , sıfırlamak ister misiniz ? Yeni Şifeyi giriniz : ");
+                newPassword=input.next();
+
+                if (newPassword.equals("java101")||newPassword.equals(password)) System.out.println("Şifreniz degiştirilemedi");
+                else
+                {
+                    System.out.println("Şifre degiştirilmiştir");
+                    password=newPassword;
+                }
+
+            }
+            else System.out.println("Hatalı Giriş - Kullanıcı adı hatalı");
+        }
+    }
+}
