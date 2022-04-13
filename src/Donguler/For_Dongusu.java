0fcee5c00d29e664221bc42a86c3f217dc068859
Umut Tosun <tosun.umutt@gmail.com>
2022-04-13 15:02:01 +0300

    for donguusu eklendi

---
+++ src/Donguler/For_Dongusu.java
@@ -0,0 +1,17 @@
+package Donguler;
+
+public class For_Dongusu {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+
+
+    }
+}
