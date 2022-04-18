a9614de0d366f9c238bdd0ecc715dcf65845bb14
Umut Tosun <tosun.umutt@gmail.com>
2022-04-18 16:24:43 +0300

    --amend

---
+++ src/KosulluIfadelerVeKodBloklari/KarsilastirmaOperatorleri.java
@@ -0,0 +1,10 @@
+package KosulluIfadelerVeKodBloklari;
+
+public class KarsilastirmaOperatorleri {
+    public static void main(String[] args) {
+        int sayiBir=5,sayiIki=10;
+        System.out.println(sayiBir==sayiIki);
+
+
+    }
+}
