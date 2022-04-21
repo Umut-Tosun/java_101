24722b426feee4f33eb828e325f280aa027dd753
Umut Tosun <tosun.umutt@gmail.com>
2022-04-21 18:02:55 +0300

    Restart metotu eksikleri kapatıldı .

--- src/BitirmeProjesi/Main.java
+++ src/BitirmeProjesi/Main.java
@@ -2,7 +2,7 @@ package BitirmeProjesi;
 
 public class Main {
     public static void main(String[] args) {
-        MineSweeper ms=new MineSweeper(10,10);
+        MineSweeper ms=new MineSweeper(1,1);
         ms.run();
     }
 }
